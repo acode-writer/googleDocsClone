a33c71dee051367dbbbe745d60cde8983d092775
Someone <souhaibou.toure122@gmail.com>
2022-10-20 22:04:13 +0000

    adding logging page.

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:googledocsclone/colors.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: Image.asset(
+            'assets/images/g-logo-2.png',
+            height: 20,
+          ),
+          label: const Text(
+            'Sign in with Google',
+            style: TextStyle(color: kBlackColor),
+          ),
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size(150, 50),
+            primary: kWhiteColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
